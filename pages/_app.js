d4f54c86e7058598d99294994982bce80419653a
Ripon Haldar <mackparker001@gmail.com>
2021-09-08 19:41:48 +0530

    'edit'

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import Layout from '../component/Layout'
-import '../styles/globals.css'
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-  )
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
